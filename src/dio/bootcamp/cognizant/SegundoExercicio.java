7b0ef37f78dfbfdc4fb4aaacdce8afe9ab2fabf5
Luiz Mori <knkmori@hotmail.com>
2022-01-07 20:04:28 -0300

    Primeiros exercícios do BootCamp Cognizant-Java-Developer

---
+++ src/dio/bootcamp/cognizant/SegundoExercicio.java
@@ -0,0 +1,58 @@
+package dio.bootcamp.cognizant;
+
+public class SegundoExercicio {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 10000;
+        //short s2 = 40000;
+
+        int i1 = 1000000000;
+        int i2 = 200000;
+
+        //long l1 = -10000000000;
+        long l2 = 200000000000000000L;
+
+        //float f1 = -1.00000000000000;
+        float f2 = 2.000000000f;
+
+        //double d1 = 1.00000000000;
+        double d2 = 242.00d;
+
+        char c1 = '\u0057';
+        char c2 = 'T';
+
+        String st1 = "Papai Noel chegou";
+        String st2 = "Eu nasci há 10 mil anos atrás";
+
+        boolean bo1 = false;
+        boolean bo2 = true;
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        System.out.println(s1);
+
+        System.out.println(i1);
+        System.out.println(i2);
+
+        System.out.println(l2);
+
+        System.out.println(f2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+        System.out.println(d2);
+
+        System.out.println(st1);
+        System.out.println(st2);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
