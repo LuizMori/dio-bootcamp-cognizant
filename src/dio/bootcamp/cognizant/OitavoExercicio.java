9f8c5f3da222b9bdc18f4637c09610d282369f99
Luiz Mori <knkmori@hotmail.com>
2022-01-10 08:40:39 -0300

    Inserido Oitavo exercício

---
+++ src/dio/bootcamp/cognizant/OitavoExercicio.java
@@ -0,0 +1,39 @@
+package dio.bootcamp.cognizant;
+
+public class OitavoExercicio {
+
+    public static void main(String[] args) {
+
+        byte b1 = -10;
+        byte b2 = 20;
+
+        short s1 = 12343;
+        short s2 = 13123;
+
+        int i1 = -4444131;
+        int i2 = 79445561;
+
+        long l1 = 45612378954355235L;
+        long l2 = 66954752541312321L;
+
+        float f1 = 3.1412413f;
+        float f2 = -1.31234f;
+
+        double d1 = 4.51413d;
+        double d2 = -3.41251d;
+
+        char c1 = 'a';
+        char c2 = 'b';
+
+        String st1 = "Abacaxi";
+        String st2 = "Melancia";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        if (f1 == d1) {
+            System.out.println("Verdade");
+        } else
+            System.out.println("Falso");
+    }
+}
